2e50593a55996942990ecf276040f91161a6350d
Jitendra <nishadji1245@gmail.com>
2021-05-15 15:48:53 -0400

    link table article

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 #  get 'welcome/home', to: 'welcome#home'
  
  get 'about' , to: 'welcome#about'
+
+ resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
